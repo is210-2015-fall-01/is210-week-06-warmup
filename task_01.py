84076c7a064d80bac87a22ccb60db2a34695fb4a
Alex Doler <aedoler@gmail.com>
2015-10-01 03:10:03 +0000

    tuple and list

---
+++ task_01.py
@@ -0,0 +1,9 @@
+#!user/bin/env python
+# -*- coding: utf-8 -*-
+"""Module that has two tuples"""
+
+ELEMENTS = (None, 'Hydrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron',
+            'Carbon')
+
+OPERATIONS_ORDER = ['Parentheses', 'Exponents', 'Multiplication', 'Division',
+                    'Addition', 'Subtraction']
