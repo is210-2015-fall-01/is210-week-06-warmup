9b4fe28bc9b19e8b95c914d1f2e2ef4082800a63
Farzad Hejazi <fhejazi@gmail.com>
2015-10-02 22:05:24 +0000

    Week 6 Warmup Assignments.

---
+++ task_02.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This is the task_02 docstring."""
+
+
+import data
+
+BALLETS = data.BALLETS
+
+del BALLETS[11]
+BALLETS[1] = 'Swan Lake'
+BALLETS.append('Herman Schmerman')
+BALLETS.extend(['Don Quixote', 'Sylvia'])
---
+++ task_03.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This is the task_03 docstring."""
+
+
+import data
+
+DIRECTIONS = data.DIRECTIONS
+
+DIRECTIONS = DIRECTIONS[:-1] + ('West', )
